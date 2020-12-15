f3387136e29b39f6502f7e7d4a789b36637d7d35
imkeines <i.klatt@mailbox.org>
2020-12-15 15:24:20 +0100

    set default values according to processMappings parameters

--- src/app/job-new/copernicus-subset/copernicus-subset.component.ts
+++ src/app/job-new/copernicus-subset/copernicus-subset.component.ts
@@ -5,6 +5,7 @@ import { ProcessMapping } from './../../processMapping.model';
 import { HttpClient } from '@angular/common/http';
 // import environment variables
 import { environment } from './../../../environments/environment';
+import {ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-copernicus-subset',
@@ -16,39 +17,64 @@ export class CopernicusSubsetComponent implements OnInit {
   // index of the process input,
   // so when a process has multiple inputobjects, the index gives information about which of the inputs you referring to
   @ViewChild('f', { static: false }) signupForm: NgForm;
+  // Reference firstNameInput variable inside Component
+
+
+
+/*   get value1() {
+    return this.defaultSatellite.nativeElement.innerHTML;
+  }
+
+  get value2() {
+    return this.defaultProductType.nativeElement.innerHTML;
+  } */
+
+
+ // ngAfterViewInit() {
+   // this.selectedSatellite = this.defaultSatellite.nativeElement.innerHTML;
+   // this.productType = this.defaultProductType.nativeElement.innerHTML;
+
+/*     this.selectedSatellite = this.value1;
+    this.productType = this.value2; */
+    // console.log('value of p: ', this.value) // value of p: Dynamic value
+  //}
+
+  @ViewChild('defaultValueSatellite', { static: false }) defaultSatellite: ElementRef<HTMLElement>;
+  @ViewChild('defaultValueProductType', { static: false }) defaultProductType: ElementRef<HTMLElement>;
 
   @Input() index: number;
   @Input() defaultProcessID: string;
   @Input() selectedTool: string;
 
+  // set default values from processMappings.json
+  @Input() defaultSat:string;
+  @Input() defaultProdType:string;
+
   @Output() selectedSatelliteEmit: EventEmitter<string> = new EventEmitter();
   @Output() selectedCloudCoverageEmit: EventEmitter<number> = new EventEmitter();
 
-  selectedSatellite = '';
-  productType='';
+  selectedSatellite:string;
+  productType:string;
 
 
   sourceType = 'CopernicusSubsetDefinition';
   cloudCoverage = 0;
 
-  // productType: string;
-
   // processMappings: empty instance for processMappings which later holds the processMappings.json
   processMappings: ProcessMapping[] = [];
-
-
 
   constructor(
     private http: HttpClient
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     // Get instance of the process mappings json file
     this.http.get<ProcessMapping[]>(environment.processMappingsPath)
       .subscribe(data => {
         this.processMappings = data;
-        console.log(this.processMappings[this.index].inputs)
       });
+    this.selectedSatellite = this.defaultSat;
+    this.productType = this.defaultProdType;
 
 
   }
